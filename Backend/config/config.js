2004dc93156e5ba1cf8bc0524750cb5e48f4acb6
Melisa <105674450+melisaapaz@users.noreply.github.com>
2024-07-11 15:45:04 -0300

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
     "development": {
-      "username": 368029,
-      "password": *Olivia2025*,
-      "database": melisaapaz_wewashdb,
-      "host": mysql-melisaapaz.alwaysdata.net,
+      "username": "368029",
+      "password": "*Olivia2025*",
+      "database": "melisaapaz_wewashdb",
+      "host": "mysql-melisaapaz.alwaysdata.net",
       "dialect": "mysql"
     },
     "test": {
-      "username": 368029,
-      "password": *Olivia2025*,
-      "database": melisaapaz_wewashdb,
-      "host": mysql-melisaapaz.alwaysdata.net,
+      "username": "368029",
+      "password": "*Olivia2025"*,
+      "database": "melisaapaz_wewashdb",
+      "host": "mysql-melisaapaz.alwaysdata.net",
       "dialect": "mysql"
     },
     "production": {
-      "username": 368029,
-      "password": *Olivia2025*,
-      "database": melisaapaz_wewashdb,
-      "host": mysql-melisaapaz.alwaysdata.net,
+      "username": "368029",
+      "password": "*Olivia2025*",
+      "database": "melisaapaz_wewashdb",
+      "host": "mysql-melisaapaz.alwaysdata.net",
       "dialect": "mysql"
     }
   };
